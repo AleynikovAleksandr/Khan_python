73d9fb1e08b2b2d138093c31579110c783ce3b8b
Alex <aleynikov.aleksandr@icloud.com>
2025-02-06 23:23:45 +1000

    work corrected

--- python_1/Task dtype - type of array elements.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-
-array = np.ones((4, 3), dtype=int)
-
-print(array)
--- python_1/Task dtype - type of array elements2.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-n = int(input("Введите положительное целое число n: "))
-
-
-array = np.arange(1, n**2 + 1, dtype=float).reshape(n, n)
-
-print(array)
