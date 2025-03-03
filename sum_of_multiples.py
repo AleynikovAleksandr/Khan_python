6fa02f2634c86489f7d2e424e89b16c45f05a252
AleynikovAleksandr <aleynikov.aleksandr@icloud.com>
2025-03-03 11:03:33 +1000

    Delete sum_of_multiples.py

--- sum_of_multiples.py
+++
@@ -1,6 +0,0 @@
-def sum_of_multiples(limit):
-    return sum(x for x in range(1, limit) if x % 3 == 0 or x % 5 == 0)
-
-limit = 1000
-result = sum_of_multiples(limit)
-print(f"The sum of all numbers from 1 to {limit} that are multiples of 3 or 5 is: {result}")
