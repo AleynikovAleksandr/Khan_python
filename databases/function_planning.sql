ceb6db782c811cafed4e940901946d9ed9378fa8
Alex <aleynikov.aleksandr@icloud.com>
2025-04-03 23:14:28 +1000

    Update function_planning.sql

--- databases/function_planning.sql
+++ databases/function_planning.sql
@@ -143,6 +143,7 @@ DELIMITER ;
 
 SELECT GetDishInfoById(6) AS 'Информация о блюде';
 
+-- Задание 5:
 DROP FUNCTION IF EXISTS GetDishNamesUpperCase;
 DELIMITER //
 
@@ -169,3 +170,177 @@ END //
 DELIMITER ;
 
 SELECT GetDishNamesUpperCase(1) AS 'Название блюда';
+
+-- Задание 6: Функция для вывода всех бронирований с датами 
+DROP FUNCTION IF EXISTS GetReservationDetails;
+DELIMITER //
+
+CREATE FUNCTION GetReservationDetails(p_reservation_id VARCHAR(50)) 
+RETURNS TEXT
+DETERMINISTIC
+READS SQL DATA
+BEGIN
+    DECLARE result TEXT DEFAULT '';
+    DECLARE res_date DATE;
+    
+    -- Получаем дату бронирования
+    SELECT 
+        visit_date
+    INTO 
+        res_date
+    FROM 
+        Reservation_Order
+    WHERE 
+        reservation_id = p_reservation_id;
+    
+    -- Формируем результат для одной записи
+    IF res_date IS NOT NULL THEN
+        SET result = CONCAT(
+            'Бронирование: ', p_reservation_id, 
+            ' | Дата: ', DATE_FORMAT(res_date, 'Год:%Y Месяц:%m День:%d')
+        );
+    ELSE
+        SET result = CONCAT('Бронирование ', p_reservation_id, ' не найдено');
+    END IF;
+    
+    RETURN result;
+END //
+
+DELIMITER ;
+
+SELECT GetReservationDetails('БР/24/0000000005') AS 'Детали бронирования';
+
+-- Задание 7: функция, которая выводит информацию о бронировании с расчетом даты отмены 
+
+DELIMITER //
+
+CREATE FUNCTION GetReservationWithCancelDate(p_reservation_id VARCHAR(50)) 
+RETURNS TEXT
+DETERMINISTIC
+READS SQL DATA
+BEGIN
+    DECLARE result TEXT DEFAULT '';
+    DECLARE visit_dt DATE;
+    DECLARE cancel_dt DATE;
+    
+    -- Получаем дату посещения
+    SELECT visit_date
+    INTO visit_dt
+    FROM Reservation_Order
+    WHERE reservation_id = p_reservation_id;
+    
+    -- Формируем результат
+    IF visit_dt IS NOT NULL THEN
+        -- Рассчитываем дату отмены (дата посещения + 21 день)
+        SET cancel_dt = DATE_ADD(visit_dt, INTERVAL 21 DAY);
+        
+        SET result = CONCAT(
+            'Номер бронирования: ', p_reservation_id, '\n',
+            'Планируемая дата посещения: ', DATE_FORMAT(visit_dt, '%Y-%m-%d'), '\n',
+            'Дата отмены брони (+21 день): ', DATE_FORMAT(cancel_dt, '%Y-%m-%d')
+        );
+    ELSE
+        SET result = CONCAT('Бронирование ', p_reservation_id, ' не найдено');
+    END IF;
+    
+    RETURN result;
+END //
+
+DELIMITER ;
+
+SELECT GetReservationWithCancelDate('БР/24/0000000001') AS 'Информация о бронировании';
+
+-- Задание 8: Функция для вывода самого дорогого блюда
+
+DELIMITER //
+
+CREATE FUNCTION GetMostExpensiveDish() 
+RETURNS TEXT
+DETERMINISTIC
+READS SQL DATA
+BEGIN
+    DECLARE result TEXT DEFAULT '';
+    DECLARE dish_name VARCHAR(100);
+    DECLARE dish_price DECIMAL(10,2);
+    
+    -- Получаем самое дорогое блюдо
+    SELECT 
+        menu_name, 
+        price
+    INTO 
+        dish_name, dish_price
+    FROM 
+        Menu
+    ORDER BY 
+        price DESC
+    LIMIT 1;
+    
+    -- Формируем результат
+    IF dish_name IS NOT NULL THEN
+        SET result = CONCAT(
+            'Самое дорогое блюдо: "', dish_name, '"\n',
+            'Цена: ', dish_price, ' руб.'
+        );
+    ELSE
+        SET result = 'Нет данных о блюдах';
+    END IF;
+    
+    RETURN result;
+END //
+
+DELIMITER ;
+
+SELECT GetMostExpensiveDish() AS 'Премиальное блюдо';
+
+-- Задание 9: Функция для вывода информации о заказе с нумерованным списком блюд
+DROP FUNCTION IF EXISTS GetOrderDetails;
+
+DELIMITER //
+
+CREATE FUNCTION GetOrderDetails(p_order_number INT) 
+RETURNS TEXT
+DETERMINISTIC
+READS SQL DATA
+BEGIN
+    DECLARE result TEXT DEFAULT '';
+    DECLARE waiter_name VARCHAR(150);
+    DECLARE dish_list TEXT DEFAULT '';
+    DECLARE num INT DEFAULT 0;
+    
+    -- Получаем ФИО сотрудника
+    SELECT CONCAT(w.last_name, ' ', w.first_name, ' ', IFNULL(w.middle_name, ''))
+    INTO waiter_name
+    FROM `Order` o
+    JOIN Waiter w ON o.employee = w.login
+    WHERE o.order_number = p_order_number;
+    
+    -- Если заказ не найден
+    IF waiter_name IS NULL THEN
+        RETURN CONCAT('Заказ №', p_order_number, ' не найден');
+    END IF;
+    
+    -- Формируем нумерованный список блюд
+    SELECT GROUP_CONCAT(
+        CONCAT(@num := @num + 1, '. ', menu_name) SEPARATOR '\n'
+    ) INTO dish_list
+    FROM (
+        SELECT m.menu_name
+        FROM Dishes_in_Order dio
+        JOIN Menu m ON dio.menu_item_id = m.menu_id
+        WHERE dio.order_number = p_order_number
+        ORDER BY m.menu_name
+    ) AS ordered_dishes, (SELECT @num := 0) AS init;
+    
+    -- Формируем итоговый результат
+    SET result = CONCAT(
+        'Сотрудник: ', waiter_name, '\n',
+        'Номер заказа: ', p_order_number, '\n',
+        'Состав заказа:\n', IFNULL(dish_list, 'Нет блюд в заказе')
+    );
+    
+    RETURN result;
+END //
+
+DELIMITER ;
+
+SELECT GetOrderDetails(1) AS 'Детали заказа';
