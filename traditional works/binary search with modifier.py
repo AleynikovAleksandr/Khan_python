2ff8c3970f31e133a378c1108c6c322fc42fe1ac
AleynikovAleksandr <aleynikov.aleksandr@icloud.com>
2025-04-08 11:43:42 +0000

    finished

--- traditional works/binary search with modifier.py
+++ traditional works/binary search with modifier.py
@@ -4,8 +4,6 @@ from typing import List, Tuple
 
 class BinarySearch:
     def __init__(self, array: List[int]):
-        if not array:
-            raise ValueError("Массив не должен быть пустым.")
         self.original_array = array
         self.sorted_array: List[Tuple[int, int]] = sorted((val, idx) for idx, val in enumerate(array))
 
@@ -19,10 +17,17 @@ class BinarySearch:
         find_first = self.original_array.count(target) > 1
         return self.binary_search(target, find_first)
 
+def validate_array(array):
+    if not isinstance(array, list):
+        raise ValueError("Массив должен быть списком.")
+    if not array:
+        raise ValueError("Массив не должен быть пустым.")
+
 if __name__ == "__main__":
     array = [0, 2, 3, 4, 5, 5, 4, 5]
-    
+
     try:
+        validate_array(array)
         binary_search = BinarySearch(array)
         print("Исходный массив:", binary_search.original_array)
 
@@ -33,6 +38,6 @@ if __name__ == "__main__":
             print(f"Индекс первого вхождения элемента {target}: {index}")
         else:
             print(f"Элемент {target} отсутствует в массиве.")
-    except ValueError:
-        print("Ошибка ввода! Завершение программы.")
+    except ValueError as e:
+        print(f"Ошибка: {e}")
         sys.exit()
