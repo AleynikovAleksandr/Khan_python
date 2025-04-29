9287505266658524233ac243b69a8b5a307513b0
AleynikovAleksandr <aleynikov.aleksandr@icloud.com>
2025-04-29 22:20:20 +1000

    modification completed

--- classic works in python/data_parsing.py
+++ classic works in python/data_parsing.py
@@ -1,22 +1,23 @@
 import sys
 
-params = {}  
+params = {}
 
 for keyval in sys.argv[1:]:
-    key, value = keyval[1:].split('=')
+    if '=' in keyval:
+        raw_key, value = keyval.split('=', 1)
+        key = raw_key.lstrip('-')  
 
-    if value.isdigit():
-        params.update({key: eval(value)})
+        if value.isdigit():
+            params[key] = int(value)
+        else:
+            try:
+                float_val = float(value)
+                params[key] = float_val
+            except ValueError:
+                params[key] = value
     else:
-        try:
-            # Пробуем преобразовать в float (включая экспоненциальную форму)
-            float_val = float(value)
-            params.update({key: float_val})
-        except ValueError:
-            # Если не число — оставим как строку
-            params.update({key: value})
+        print(f"Пропущен некорректный аргумент: {keyval}")
 
-for k, v in params.items():
-    print(f"{k} = {v}({type(v)})")
-    
-#python data_parsing.py -var1=100 -var2=3.14 -var3=1e-21 -var4=Булки -var5=ещё -var6=мягких
+print('\n'.join(f"{k.ljust(max(len(k) for k in params))} : {str(v).ljust(10)} > ({type(v).__name__})" for k, v in params.items()))
+
+#python3 data_parsing.py --var1=100 --var2=3.14 --var3=1e-21 --var4=good
