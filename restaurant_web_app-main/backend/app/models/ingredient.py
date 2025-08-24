d6d0fd6c36b426f147846b9642be79646a587ae6
AleynikovAleksandr <aleynikov.aleksandr@icloud.com>
2025-08-24 15:29:11 +1000

    Delete restaurant_web_app-main directory

--- restaurant_web_app-main/backend/app/models/ingredient.py
+++
@@ -1,8 +0,0 @@
-from app import db
-
-class Ingredient(db.Model):
-    __tablename__ = 'Ingredient'
-    ingredient_id = db.Column(db.Integer, primary_key=True)
-    ingredient_name = db.Column(db.String(100), nullable=False)
-
-    composition = db.relationship("Composition", back_populates="ingredient")
