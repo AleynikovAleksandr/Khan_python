9b4223e01930b2440e4b3141bb78236ff2593ac0
AleynikovAleksandr <aleynikov.aleksandr@icloud.com>
2026-04-14 21:55:04 +1000

    studying engines

--- clickhouse_local/clickhouse_data/store/3b3/3b32c956-e940-43d7-98c8-d3f1e4f8379d/Shipper_Dim.sql
+++
@@ -1,9 +0,0 @@
-ATTACH TABLE _ UUID '74184ca2-b075-4aeb-bc70-7dd358067ccf'
-(
-    `ShipperKey` UInt64,
-    `ShipperID` Int32,
-    `ShipperName` String
-)
-ENGINE = MergeTree
-ORDER BY ShipperKey
-SETTINGS index_granularity = 8192
